a9fb36d81ee169e23bbc11c3960e3de3e5a44688
Alex Esson <Ghostshadow1222@users.noreply.github.com>
2025-05-19 18:12:37 -0700

    Implemented the decisions for the program to track what the user has chosen for a main course, along with any sides and calculates the subtotal, tax, and order total. Also implemented functionality for the Exit button to close the program.

--- LunchOrderAppGit/Form1.cs
+++ LunchOrderAppGit/Form1.cs
@@ -13,16 +13,46 @@ namespace LunchOrderAppGit
         public Form1()
         {
             InitializeComponent();
+            
         }
 
         private void OrderBtn_Click(object sender, EventArgs e)
         {
+            decimal subtotal = 0m;
 
+            if (HamburgerRdo.Checked)
+            {
+                subtotal = HAMBURGER_PRICE;
+                if (HamburgerSide1Cbo.Checked) subtotal += HAMBURGER_ADDON_PRICE;
+                if (HamburgerSide2Cbo.Checked) subtotal += HAMBURGER_ADDON_PRICE;
+                if (HamburgerSide3Cbo.Checked) subtotal += HAMBURGER_ADDON_PRICE;
+            }
+            else if (PizzaRdo.Checked)
+            {
+                subtotal = PIZZA_PRICE;
+                if (PizzaSide1Cbo.Checked) subtotal += PIZZA_ADDON_PRICE;
+                if (PizzaSide2Cbo.Checked) subtotal += PIZZA_ADDON_PRICE;
+                if (PizzaSide3Cbo.Checked) subtotal += PIZZA_ADDON_PRICE;
+            }
+            else if (SaladRdo.Checked)
+            {
+                subtotal = SALAD_PRICE;
+                if (SaladSide1Cbo.Checked) subtotal += SALAD_ADDON_PRICE;
+                if (SaladSide2Cbo.Checked) subtotal += SALAD_ADDON_PRICE;
+                if (SaladSide3Cbo.Checked) subtotal += SALAD_ADDON_PRICE;
+            }
+
+            decimal tax = subtotal * TAX_RATE;
+            decimal total = subtotal + tax;
+
+            SubtotalTxt.Text = subtotal.ToString("c");
+            TaxTxt.Text = tax.ToString("c");
+            OrderTotalTxt.Text = total.ToString("c");
         }
 
         private void ExitBtn_Click(object sender, EventArgs e)
         {
-
+            Application.Exit();
         }
     }
 }
